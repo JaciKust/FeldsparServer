a56c66bd8cff62e90483f868a13835723ef4c47a
Jaci Kust <jaci.kust@protonmail.com>
2021-09-25 13:27:51 -0600

    Add project files.

---
+++ FeldsparServer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace FeldsparServer
+{
+    class Program
+    {
+        public static void Main()
+        {
+			using (var server = new ResponseSocket())
+			{
+				server.Bind("tcp://*:5556");
+				Console.WriteLine("Waiting...");
+				Console.WriteLine();
+				string msg = server.ReceiveFrameString();
+				Console.WriteLine("From Client: {0}", msg);
+				Console.WriteLine();
+				server.SendFrame("ack");
+			}
+
+			// Send
+			//using (var client = new RequestSocket())
+			//{
+			//	client.Connect("tcp://192.168.0.158:5555");
+			//	client.SendFrame("Hello");
+			//	var msg = client.ReceiveFrameString();
+			//	Console.WriteLine("From Server: {0}", msg);
+			//}
+		}
+    }
+}
---
+++ FeldsparServer/State/BaseState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeldsparServer.State
+{
+	public abstract class BaseState : IState
+	{
+	}
+}
