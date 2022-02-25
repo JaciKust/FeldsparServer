ebdb341949ad3f71906de845d07d7baf9a72b6d0
Jaci Kust <jaci.kust@protonmail.com>
2022-02-24 20:02:13 -0700

    Added lifx runner for testing.

---
+++ LifxRunner/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Common;
+using FeldsparServer.Interactable;
+
+namespace LifxRunner
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			List<Color> colors = new List<Color>{
+				Colors.Red,
+				Colors.Green,
+				Colors.Blue,
+				Colors.Magenta,
+				Colors.Cyan,
+				Colors.Yellow
+			};
+
+			List<LifxLamp> lifxLights = new List<LifxLamp>{
+				LifxBulbs.RedLamp,
+				LifxBulbs.YellowLamp,
+				LifxBulbs.WhiteLamp,
+				LifxBulbs.BlackLamp,
+				LifxBulbs.SilverLamp
+			};
+
+			int transitionTime = 5;
+
+			Console.WriteLine("Done");
+			while (true)
+			{
+				foreach (var light in lifxLights)
+				{
+					var random = new Random();
+					int randomEntry = random.Next(colors.Count);
+					Color randomColor = colors.ElementAt(randomEntry);
+
+					light.TurnOn(randomColor, transitionTime);
+					Thread.Sleep(transitionTime * 1000);
+				}
+			}
+		}
+	}
+}
